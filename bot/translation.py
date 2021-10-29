aaa6bc38f57686052ff118252082b09839908f16
github237acc <93255823+github237acc@users.noreply.github.com>
2021-10-29 10:45:10 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -11,49 +11,10 @@ class Translation(object):
 
 എന്നെ മറ്റൊരു ഗ്രൂപ്പിലും ചേർക്കാൻ ശ്രമിക്കരുത്
 
-For More Details Click Help Button Below..
-@i_creator_bot
+To join our group click👉 @ask_movie_latest
+To join our channel click👉 @filesharebotusers
+
+For more information click👇
+                 @i_creator_bot
 </i>"""    
     
-    HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
-
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-@i_creator_bot
-"""
-    
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> ASK MOVIE LATEST </code>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/i_creator_bot">@i_creator_bot</a></i></b>
-
-<b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-"""
